22bc68b8f560e57823be02f00fadd1b9a40ccebb
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-07-11 22:35:08 +0100

    WIP: 443 string compression

---
+++ leetcode/443_String_Compression.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75
+from typing import List
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        pass
+
+
+print(Solution().compress(["a","a","b","b","c","c","c"]))
+print(Solution().compress(["a"]))
+print(Solution().compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]))

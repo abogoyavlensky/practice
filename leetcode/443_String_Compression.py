aaefa010db41b0fc383672aa52c532752f0ceec0
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-07-13 00:31:26 +0100

    Works with second list

--- leetcode/443_String_Compression.py
+++ leetcode/443_String_Compression.py
@@ -3,7 +3,26 @@ from typing import List
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        pass
+        chars_len = len(chars)
+        result = []
+        start = 0 
+        start_char = chars[0]
+        start_char_len = 0
+
+        for i in range(chars_len):
+            if start_char == chars[i]:
+                start_char_len += 1
+
+            if start_char != chars[i] or i == chars_len - 1:
+                result += [start_char]
+                if start_char_len > 1:
+                    result += list(str(start_char_len))
+                start_char = chars[i]
+                start_char_len = 1
+
+        chars = result
+        print(chars)
+        return len(chars)
 
 
 print(Solution().compress(["a","a","b","b","c","c","c"]))

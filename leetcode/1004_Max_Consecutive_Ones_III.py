f9d7ba9391a01fbfcbc19dd4dda04d27f3c210e4
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-07-21 00:35:47 +0100

    Add translated article about kamal

---
+++ leetcode/1004_Max_Consecutive_Ones_III.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/max-consecutive-ones-iii/description/?envType=study-plan-v2&envId=leetcode-75
+# Topcis: Array, Binary Search, Sliding Window, Prefix Sum
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        pass
+        
+
+print(Solution().longestOnes(nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2))

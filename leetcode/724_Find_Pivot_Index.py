e1b3ca01d5cdbaad52cc3a2dc8b2bf7a58378785
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-08-27 08:49:42 +0100

    Add solution for 724

--- leetcode/724_Find_Pivot_Index.py
+++ leetcode/724_Find_Pivot_Index.py
@@ -3,7 +3,30 @@
 
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
-        pass
+        len_nums = len(nums)
+        left_sum_arr = []
+        right_sum_arr = []
+
+        left_sum = 0
+
+        for i in nums:
+            left_sum_arr.append(left_sum)
+            left_sum += i
+
+        right_sum = 0
+
+        for i in range(len_nums - 1, -1, -1):
+            right_sum_arr.append(right_sum)
+            right_sum += nums[i]
+
+        right_sum_arr.reverse()
+
+        for i in range(len_nums):
+            if left_sum_arr[i] == right_sum_arr[i]:
+                return i
+        
+        return -1
+
 
 
 print(Solution().pivotIndex(nums = [1,7,3,6,5,6]))

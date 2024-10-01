1d69df3ab4c88be01eba71f7bb55bcfc8241400a
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2024-10-01 11:25:39 +0100

    Add new LC-875

---
+++ leetcode/875_Koko_Eating_Bananas.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/koko-eating-bananas/description/?envType=study-plan-v2&envId=leetcode-75
+# Topics: Array, Binary Search
+
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        pass
+
+
+print(Solution().minEatingSpeed(piles = [3,6,7,11], h = 8))
+print(Solution().minEatingSpeed(piles = [30,11,23,4,20], h = 5))
+print(Solution().minEatingSpeed(piles = [30,11,23,4,20], h = 6))
